00be32011943bb90b0c826c3e01faf9e7705f650
Sainadh Vatturi <sainadhvatturi@gmail.com>
2023-11-30 08:15:47 +0530

    GCD Problem

--- src/mathematicsmod2/PrimeNumber.java
+++ src/mathematicsmod2/PrimeNumber.java
@@ -49,7 +49,7 @@ public class PrimeNumber {
             return false;
         }
         for(int i = 5; i * i <= n ; i = i + 6){
-            if(n%i == 0 || n %i+2==0){
+            if(n%i == 0 || n %(i+2)==0){
                 return false;
             }
         }
