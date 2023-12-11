398098229d97198fcacccb5a4b7592710df45765
Sainadh Vatturi <sainadhvatturi@gmail.com>
2023-12-11 10:33:16 +0530

    completed conditions loops

---
+++ src/javakunalas4/intermediate/ReverseString.java
@@ -0,0 +1,17 @@
+package javakunalas4.intermediate;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        String reversedName = "";
+        int nameLength = name.length();
+        for(int i = 0; i < nameLength; i++){
+            reversedName += name.charAt(nameLength-(i+1));
+        }
+        System.out.println(reversedName);
+    }
+
+}
