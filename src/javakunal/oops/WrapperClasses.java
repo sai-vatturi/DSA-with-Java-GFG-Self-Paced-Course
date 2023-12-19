5a1b9def22aca7129769c12d2b758c23fcf3137d
Sainadh V <sainadhvatturi@gmail.com>
2023-12-19 15:54:46 +0530

    oop class 1 lecture practice

---
+++ src/javakunal/oops/WrapperClasses.java
@@ -0,0 +1,11 @@
+package javakunal.oops;
+
+public class WrapperClasses {
+    public static void main(String[] args) {
+        Integer a = 15;
+        Integer b = 25;
+        System.out.println(a + " " + b);
+        final int LASTONE = 14;
+        System.out.println(LASTONE);
+    }
+}
