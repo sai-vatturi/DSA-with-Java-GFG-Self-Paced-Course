cd66c8c32e848dc84193080651d3731235590507
Sainadh V <sainadhvatturi@gmail.com>
2024-01-12 07:37:25 +0530

    abstract classes

---
+++ src/javakunal/oops/abstractDemo/Main.java
@@ -0,0 +1,13 @@
+package javakunal.oops.abstractDemo;
+
+public class Main {
+    public static void main(String[] args) {
+        Son son = new Son();
+        son.career();
+        son.partner();
+        Daughter daughter = new Daughter();
+        daughter.career();
+        daughter.partner();
+        Parent.hello();
+    }
+}
---
+++ src/javakunal/oops/abstractDemo/Parent.java
@@ -0,0 +1,9 @@
+package javakunal.oops.abstractDemo;
+
+public abstract class Parent {
+    abstract void career();
+    abstract void partner();
+    static void hello(){
+        System.out.println("Hey ya!");
+    }
+}
