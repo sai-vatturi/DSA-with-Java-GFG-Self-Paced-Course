23c4fd51a3b5f1139ba6970a7cb15da3ea260e34
Sainadh V <sainadhvatturi@gmail.com>
2024-01-17 16:07:55 +0530

    bs problems

---
+++ src/javakunalclass/binarysearch/SmallestLetter.java
@@ -0,0 +1,26 @@
+package javakunalclass.binarysearch;
+
+public class SmallestLetter {
+    public static void main(String[] args) {
+        char[] arr = {'x', 'x', 'y', 'y'};
+        System.out.println(nextGreatestLetter(arr, 'x'));
+    }
+
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
+
+
+
+
