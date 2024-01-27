e15234011b65f3d832e510e62efa882496fd4a92
Sainadh V <sainadhvatturi@gmail.com>
2024-01-27 22:51:04 +0530

    cyclic sort

--- src/javakunalclass/sorting/BubbleSort.java
+++ src/javakunalclass/sorting/BubbleSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 5, 4, 3};
         System.out.println(Arrays.toString(arr));
         bubble(arr);
         System.out.println(Arrays.toString(arr));
