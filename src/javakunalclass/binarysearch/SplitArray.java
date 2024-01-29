bb573c06ed5b18b79e0d6811c85223ca0a8c76f1
Sainadh V <sainadhvatturi@gmail.com>
2024-01-29 22:06:41 +0530

    recursion lecture

---
+++ src/javakunalclass/binarysearch/SplitArray.java
@@ -0,0 +1,9 @@
+package javakunalclass.binarysearch;
+
+public class SplitArray {
+    public static void main(String[] args) {
+        int[] arr = {7, 2, 5, 10, 8};
+
+    }
+
+}
---
+++ src/javakunalclass/recursion/NumbersExampleRecursion.java
@@ -0,0 +1,16 @@
+package javakunalclass.recursion;
+
+public class NumbersExampleRecursion {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    private static void print(int n) {
+        if (n == 5) {
+            System.out.println(5);
+            return;
+        }
+        System.out.println(n);
+        print(n + 1); // Tail recursion
+    }
+}
